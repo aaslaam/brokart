5f78bb9b8a669bb70f2df43cacf6abb4099f779b
aaslaam <muhammedaslamks8@gmail.com>
2025-07-26 11:37:02 +0530

    registred order model admin

--- orders/admin.py
+++ orders/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 from .models import *
 
-# Removed duplicate Product registration to avoid AlreadyRegistered error
-
+admin.site.register(Order)
+admin.site.register(OrderedItem)
